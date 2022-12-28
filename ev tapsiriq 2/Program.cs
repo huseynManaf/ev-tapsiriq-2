9650435f3cc62405ac8cca80393da3c35d3113a4
huseynManaf <huseynim@code.edu.az>
2022-12-28 00:34:53 -0800

    init

---
+++ ev tapsiriq 2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ev_tapsiriq_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin");
+            int e = int.Parse(Console.ReadLine());
+
+            int a, b, c, d, s;
+            a = 2345 / 1000;
+            b = 34 / 10;
+            c = 234%10;
+            d = 2345 % 10;
+            s = 1 * a + 2 * b + 3 * c + 4 * d;
+            Console.WriteLine(s);
+        }
+    }
+}
